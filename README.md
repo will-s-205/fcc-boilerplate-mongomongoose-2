ff24a662afd7696677c674610d151be15b855ce4
William S <97144951+will-s-205@users.noreply.github.com>
2023-05-15 18:21:59 -0700

    project added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# fcc-boilerplate-mongomongoose-2
+# MongoDB and Mongoose Challenges
+
+This is the boilerplate for the MongoDB and Mongoose lessons. Instructions for completing these lessons start at https://www.freecodecamp.org/learn/apis-and-microservices/mongodb-and-mongoose/
---
+++ myApp.js
@@ -0,0 +1,101 @@
+require('dotenv').config();
+
+/** 1) Install & Set up mongoose */
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI);
+
+// 2 Create a Model
+const personSchema = new mongoose.Schema({
+  name: {
+  type: String,
+  required: true
+  },
+  age: {
+    type: Number
+  },
+  favoriteFoods:
+    [String]
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+// 3 Create and Save a Record of a Model
+var createAndSavePerson = (done) => {
+  var janeFonda = new Person(
+    {name: "Jane Fonda", 
+     age: 84, 
+     favoriteFoods: ["eggs", "fish", "fresh fruit"]
+    });
+
+  janeFonda.save(function(err, data) {
+    if (err) return console.error(err);
+    done(null, data)
+  });
+};
+
+// const createAndSavePerson = (done) => {
+//   done(null /*, data*/);
+// };
+
+// X other stuff
+const createManyPeople = (arrayOfPeople, done) => {
+  done(null /*, data*/);
+};
+
+const findPeopleByName = (personName, done) => {
+  done(null /*, data*/);
+};
+
+const findOneByFood = (food, done) => {
+  done(null /*, data*/);
+};
+
+const findPersonById = (personId, done) => {
+  done(null /*, data*/);
+};
+
+const findEditThenSave = (personId, done) => {
+  const foodToAdd = "hamburger";
+
+  done(null /*, data*/);
+};
+
+const findAndUpdate = (personName, done) => {
+  const ageToSet = 20;
+
+  done(null /*, data*/);
+};
+
+const removeById = (personId, done) => {
+  done(null /*, data*/);
+};
+
+const removeManyPeople = (done) => {
+  const nameToRemove = "Mary";
+
+  done(null /*, data*/);
+};
+
+const queryChain = (done) => {
+  const foodToSearch = "burrito";
+
+  done(null /*, data*/);
+};
+
+/** **Well Done !!**
+/* You completed these challenges, let's go celebrate !
+ */
+
+//----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
+
+exports.PersonModel = Person;
+exports.createAndSavePerson = createAndSavePerson;
+exports.findPeopleByName = findPeopleByName;
+exports.findOneByFood = findOneByFood;
+exports.findPersonById = findPersonById;
+exports.findEditThenSave = findEditThenSave;
+exports.findAndUpdate = findAndUpdate;
+exports.createManyPeople = createManyPeople;
+exports.removeById = removeById;
+exports.removeManyPeople = removeManyPeople;
+exports.queryChain = queryChain;
