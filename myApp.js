69fe6fb6b77714d44a3629ca678d1635416692ec
will-s-205 <97144951+will-s-205@users.noreply.github.com>
2023-05-16 19:22:23 +0000

    8 Perform Classic Updates by Running Find, Edit, then Save

--- myApp.js
+++ myApp.js
@@ -72,10 +72,23 @@ const findPersonById = (personId, done) => {
   });
 };
 
+// 8 Perform Classic Updates by Running Find, Edit, then Save
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
 
-  done(null /*, data*/);
+  // .findById() method to find a person by _id with the parameter personId as search key. 
+  Person.findById(personId, function(err, person) {
+    if (err) return console.log(err);
+
+    // Array.push() method to add "hamburger" to the list of the person's favoriteFoods
+    person.favoriteFoods.push(foodToAdd);
+
+    // and inside the find callback - save() the updated Person.
+    person.save((err, updatedPerson) => {
+      if (err) return console.log(err);
+      done(null, updatedPerson)
+    })
+  })
 };
 
 const findAndUpdate = (personName, done) => {
